71901aadeae6d39dd4c023c5f9a283f74eb3837a
Priyesh Gandhi <priyeshgandhi@Priyeshs-MacBook-Air.local>
2025-10-19 16:55:13 +0530

    Revert credentials to root folder - v1.4.18 structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { SimpleTestNode } = require('./simple-test-node.js');
 const { PeakAiNode } = require('./peak-ai-node.js');
-const { PeakAiApi } = require('./credentials/peak-ai-credentials.js');
+const { PeakAiApi } = require('./peak-ai-credentials.js');
 
 module.exports = {
     nodeTypes: {
