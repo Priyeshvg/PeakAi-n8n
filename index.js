e98a2991911231954a957c491286722b4e009528
Priyesh Gandhi <priyeshgandhi@Priyeshs-MacBook-Air.local>
2025-10-19 16:16:37 +0530

    Revert completely to v1.4.9 structure

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const { SimpleTestNode } = require('./simple-test-node.js');
-const { PeakAiNode } = require('./peak-ai-node.js');
-const { PeakAiApi } = require('./peak-ai-credentials.js');
-
 module.exports = {
-    nodeTypes: {
-        simpleTest: SimpleTestNode,
-        peakAi: PeakAiNode
-    },
-    credentialTypes: {
-        peakAiApi: PeakAiApi
-    }
+    nodes: [
+        './simple-test-node.js',
+        './peak-ai-node.js'
+    ],
+    credentials: [
+        './peak-ai-credentials.js'
+    ]
 };
